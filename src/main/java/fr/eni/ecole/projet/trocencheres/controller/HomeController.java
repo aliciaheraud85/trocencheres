b064fe1593f5c9a93e358e7a5fd974063fb5d242
aliciaheraud85 <alicia.heraud@gmail.com>
2025-09-17 11:48:26 +0200

    end of auction list

--- src/main/java/fr/eni/ecole/projet/trocencheres/controller/HomeController.java
+++ src/main/java/fr/eni/ecole/projet/trocencheres/controller/HomeController.java
@@ -1,12 +1,14 @@
 package fr.eni.ecole.projet.trocencheres.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import fr.eni.ecole.projet.trocencheres.bo.Categorie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import fr.eni.ecole.projet.trocencheres.service.ArticleAVendreService;
 import fr.eni.ecole.projet.trocencheres.bo.ArticleAVendre;
+import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -28,6 +30,9 @@ public class HomeController {
 
         List<ArticleAVendre> lstArticles = articleAVendreService.getAuctionList();
         model.addAttribute("articles", lstArticles);
+
+        List<Categorie> lstCategories = articleAVendreService.getCategoriesList();
+        model.addAttribute("categories", lstCategories);
         return "index";
     }
 
@@ -35,5 +40,20 @@ public class HomeController {
     public String helloWorld(){
         return "hello";
     }
+
+    @GetMapping("/auction-details")
+    public void auctionDetails(@RequestParam int id){
+        if(id > 0){
+            ArticleAVendre articleById = articleAVendreService.getArticleAVendre(id);
+            if(articleById != null)
+                System.out.println(articleById);
+            else
+                System.out.println("This article does not exist");
+
+        }else{
+            System.out.println("This id does not exist");
+        }
+    }
+
 
 }
--- src/main/java/fr/eni/ecole/projet/trocencheres/service/ArticleAVendreService.java
+++ src/main/java/fr/eni/ecole/projet/trocencheres/service/ArticleAVendreService.java
@@ -1,22 +1,36 @@
 package fr.eni.ecole.projet.trocencheres.service;
 
+import fr.eni.ecole.projet.trocencheres.bo.Categorie;
+import fr.eni.ecole.projet.trocencheres.repository.CategorieRepository;
 import org.springframework.stereotype.Service;
 import fr.eni.ecole.projet.trocencheres.bo.ArticleAVendre;
 
 import fr.eni.ecole.projet.trocencheres.repository.ArticleAVendreRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleAVendreService {
 
     private final ArticleAVendreRepository articleRepository;
+    private final CategorieRepository categorieRepository;
 
-    public ArticleAVendreService(ArticleAVendreRepository articleRepository) {
+    public ArticleAVendreService(ArticleAVendreRepository articleRepository,  CategorieRepository categorieRepository) {
         this.articleRepository = articleRepository;
+        this.categorieRepository = categorieRepository;
     }
 
     public List<ArticleAVendre> getAuctionList(){
         return articleRepository.findAll();
     }
+
+    public List<Categorie> getCategoriesList(){
+        return categorieRepository.findAll();
+    }
+
+    public ArticleAVendre getArticleAVendre(int id){
+        return articleRepository.findById(id).orElse(null);
+    }
+
 }
